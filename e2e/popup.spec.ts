5aa9b910ea1b4dc7376f3d73a57246ad295d0241
noriapi <70106808+noriapi@users.noreply.github.com>
2023-12-28 22:51:52 +0900

    chore(test): add minimal e2e test for chrome (#24)
    * chore(test): add minimal e2e test for chrome
    
    * chore(test): fix chrome to run in headless mode

---
+++ e2e/popup.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test } from "./fixtures";
+import { openPopup } from "./pages/popup";
+
+test("subs should be checked", async ({ page, extensionId }) => {
+  const popup = await openPopup(page, extensionId);
+
+  await expect(popup.getSubs()).toBeChecked();
+});
+
+test("views should be checked", async ({ page, extensionId }) => {
+  const popup = await openPopup(page, extensionId);
+
+  await expect(popup.getViews()).toBeChecked();
+});
+
+test("video likes should be checked", async ({ page, extensionId }) => {
+  const popup = await openPopup(page, extensionId);
+
+  await expect(popup.getVideoLikes()).toBeChecked();
+});
+
+test("comment likes should be checked", async ({ page, extensionId }) => {
+  const popup = await openPopup(page, extensionId);
+
+  await expect(popup.getCommentLikes()).toBeChecked();
+});
---
+++ e2e/video.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from "./fixtures";
+import { openVideo } from "./pages/video";
+
+test("views should not be visible", async ({ page }) => {
+  const video = await openVideo(page);
+
+  await expect(video.views()).not.toBeVisible();
+});
+
+test("subscribers should not be visible", async ({ page }) => {
+  const video = await openVideo(page);
+
+  await expect(video.subscribers()).not.toBeVisible();
+});
+
+test("video likes should not be visible", async ({ page }) => {
+  const video = await openVideo(page);
+
+  await expect(video.videoLikes()).not.toBeVisible();
+});
+
+test("comment likes should not be visible", async ({ page }) => {
+  const video = await openVideo(page);
+
+  await expect(video.commentLikes()).not.toBeVisible();
+});
+
+test("related videos views should not be visible", async ({ page }) => {
+  const video = await openVideo(page);
+
+  await expect(video.relatedVideoViews().first()).not.toBeVisible();
+});
