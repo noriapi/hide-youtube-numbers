d3389a1d923d1e9c285398782593e74c7fe9935b
noriapi <70106808+noriapi@users.noreply.github.com>
2023-12-28 12:01:19 +0900

    fix(build): fix manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,25 @@
 import Solid from "vite-plugin-solid";
 import { defineConfig } from "wxt";
 
+const manifestBase = {
+  name: "Hide YouTube Numbers",
+  permissions: ["storage"],
+  author: "noriapi",
+  homepage_url: "https://github.com/noriapi/hide-youtube-numbers",
+};
+
+const manifestV2 = {
+  ...manifestBase,
+  browser_specific_settings: {
+    gecko: {
+      id: "hide-youtube-numbers@noriapi.addon",
+      strict_min_version: "57.0",
+    },
+  },
+};
+
+const manifestV3 = manifestBase;
+
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   vite: () => ({
@@ -9,18 +28,7 @@ export default defineConfig({
     },
     plugins: [Solid()],
   }),
-  manifest: {
-    name: "Hide YouTube Numbers",
-    permissions: ["storage"],
-    author: "noriapi",
-    homepage_url: "https://github.com/noriapi/hide-youtube-numbers",
-    browser_specific_settings: {
-      gecko: {
-        id: "hide-youtube-numbers@noriapi.addon",
-        strict_min_version: "57.0",
-      },
-    },
-  },
+  manifest: (env) => (env.manifestVersion === 2 ? manifestV2 : manifestV3),
   srcDir: "src",
   imports: false,
 });
