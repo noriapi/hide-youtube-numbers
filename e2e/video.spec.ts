1ac6c94f2e54ffdc3c731d3ad000f57c6c075f0f
noriapi <70106808+noriapi@users.noreply.github.com>
2023-12-29 18:10:14 +0900

    chore(test): attach screenshots to test report

--- e2e/video.spec.ts
+++ e2e/video.spec.ts
@@ -1,32 +1,53 @@
+import { Locator, TestInfo } from "@playwright/test";
+
 import { expect, test } from "./fixtures";
 import { openVideo } from "./pages/video";
 
-test("views should not be visible", async ({ page }) => {
+const takeSs = async (testInfo: TestInfo, elem: Locator, name: string) => {
+  const ss = await elem.screenshot();
+  return testInfo.attach(name, {
+    body: ss,
+    contentType: "image/png",
+  });
+};
+
+test("views should not be visible", async ({ page }, testInfo) => {
   const video = await openVideo(page);
 
+  await takeSs(testInfo, video.views(), "video-views-hidden");
   await expect(video.views()).not.toBeVisible();
 });
 
-test("subscribers should not be visible", async ({ page }) => {
+test("subscribers should not be visible", async ({ page }, testInfo) => {
   const video = await openVideo(page);
 
+  await takeSs(testInfo, video.subscribers(), "video-subscribers-hidden");
   await expect(video.subscribers()).not.toBeVisible();
 });
 
-test("video likes should not be visible", async ({ page }) => {
+test("video likes should not be visible", async ({ page }, testInfo) => {
   const video = await openVideo(page);
 
+  await takeSs(testInfo, video.videoLikes(), "video-videolikes-hidden");
   await expect(video.videoLikes()).not.toBeVisible();
 });
 
-test("comment likes should not be visible", async ({ page }) => {
+test("comment likes should not be visible", async ({ page }, testInfo) => {
   const video = await openVideo(page);
 
+  await takeSs(testInfo, video.commentLikes(), "video-commentlikes-hidden");
   await expect(video.commentLikes()).not.toBeVisible();
 });
 
-test("related videos views should not be visible", async ({ page }) => {
+test("related videos views should not be visible", async ({
+  page,
+}, testInfo) => {
   const video = await openVideo(page);
 
+  await takeSs(
+    testInfo,
+    video.relatedVideoViews(),
+    "video-relatedvideoviews-hidden",
+  );
   await expect(video.relatedVideoViews().first()).not.toBeVisible();
 });
