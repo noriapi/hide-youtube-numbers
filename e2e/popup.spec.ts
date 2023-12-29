de9c3131bca1807128004a9deb852379db6575e1
noriapi <70106808+noriapi@users.noreply.github.com>
2023-12-30 03:57:28 +0900

    chore(test): change video page tests (#37)

--- e2e/popup.spec.ts
+++ e2e/popup.spec.ts
@@ -4,23 +4,23 @@ import { openPopup } from "./pages/popup";
 test("subs should be checked", async ({ page, extensionId }) => {
   const popup = await openPopup(page, extensionId);
 
-  await expect(popup.subs).toBeChecked();
+  await expect(popup.checkbox.subscribers).toBeChecked();
 });
 
 test("views should be checked", async ({ page, extensionId }) => {
   const popup = await openPopup(page, extensionId);
 
-  await expect(popup.views).toBeChecked();
+  await expect(popup.checkbox.views).toBeChecked();
 });
 
 test("video likes should be checked", async ({ page, extensionId }) => {
   const popup = await openPopup(page, extensionId);
 
-  await expect(popup.videoLikes).toBeChecked();
+  await expect(popup.checkbox.videoLikes).toBeChecked();
 });
 
 test("comment likes should be checked", async ({ page, extensionId }) => {
   const popup = await openPopup(page, extensionId);
 
-  await expect(popup.commentLikes).toBeChecked();
+  await expect(popup.checkbox.commentLikes).toBeChecked();
 });
