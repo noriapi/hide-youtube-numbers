c0e4e6c683e8a7a727596f1389d40d1b623d3e9e
noriapi <70106808+noriapi@users.noreply.github.com>
2023-12-29 23:39:39 +0900

    chore(test): change video page tests

--- e2e/pages/popup.ts
+++ e2e/pages/popup.ts
@@ -3,11 +3,20 @@ import { Page } from "@playwright/test";
 export async function openPopup(page: Page, extensionId: string) {
   await page.goto(`chrome-extension://${extensionId}/popup.html`);
 
-  const popup = {
+  const checkbox = {
     views: page.getByRole("checkbox", { name: "Views" }),
-    subs: page.getByRole("checkbox", { name: "Subscribers" }),
+    subscribers: page.getByRole("checkbox", { name: "Subscribers" }),
     videoLikes: page.getByRole("checkbox", { name: "Video likes" }),
     commentLikes: page.getByRole("checkbox", { name: "Comment likes" }),
+  };
+
+  const popup = {
+    checkbox,
+    uncheckAll: async () => {
+      for (const c of Object.values(checkbox)) {
+        await c.uncheck();
+      }
+    },
   };
 
   return popup;
--- e2e/video.spec.ts
+++ e2e/video.spec.ts
@@ -1,32 +1,67 @@
+import { BrowserContext, Locator, Page } from "@playwright/test";
+
 import { expect, test } from "./fixtures";
+import { openPopup } from "./pages/popup";
 import { openVideo } from "./pages/video";
 
-test("views should not be visible", async ({ page }) => {
-  const video = await openVideo(page);
+test.describe("visiblity conform options", () => {
+  const prepare = async (
+    popupPage: Page,
+    extensionId: string,
+    context: BrowserContext,
+  ) => {
+    const popup = await openPopup(popupPage, extensionId);
+    await popup.uncheckAll();
 
-  await expect(video.views).not.toBeVisible();
-});
+    const videoPage = await context.newPage();
+    const video = await openVideo(videoPage);
 
-test("subscribers should not be visible", async ({ page }) => {
-  const video = await openVideo(page);
+    return {
+      popupPage,
+      popup,
+      videoPage,
+      video,
+    };
+  };
 
-  await expect(video.subscribers).not.toBeVisible();
-});
+  type P = Awaited<ReturnType<typeof prepare>>;
 
-test("video likes should not be visible", async ({ page }) => {
-  const video = await openVideo(page);
+  const testCases: [string, (p: P) => Locator, (p: P) => Locator][] = [
+    ["views", ({ video }) => video.views, ({ popup }) => popup.checkbox.views],
+    [
+      "subscribers",
+      ({ video }) => video.subscribers,
+      ({ popup }) => popup.checkbox.subscribers,
+    ],
+    [
+      "videoLikes",
+      ({ video }) => video.videoLikes,
+      ({ popup }) => popup.checkbox.videoLikes,
+    ],
+    [
+      "commentLikes",
+      ({ video }) => video.commentLikes.first(),
+      ({ popup }) => popup.checkbox.commentLikes,
+    ],
+    [
+      "relatedVideosViews",
+      ({ video }) => video.relatedVideoViews.first(),
+      ({ popup }) => popup.checkbox.views,
+    ],
+  ];
 
-  await expect(video.videoLikes).not.toBeVisible();
-});
+  testCases.forEach(([label, getTarget, getCheckbox]) => {
+    test(label, async ({ page, extensionId, context }) => {
+      const p = await prepare(page, extensionId, context);
+      const target = getTarget(p);
+      const targetCheckbox = getCheckbox(p);
 
-test("comment likes should not be visible", async ({ page }) => {
-  const video = await openVideo(page);
-
-  await expect(video.commentLikes).not.toBeVisible();
-});
-
-test("related videos views should not be visible", async ({ page }) => {
-  const video = await openVideo(page);
-
-  await expect(video.relatedVideoViews.first()).not.toBeVisible();
+      await target.scrollIntoViewIfNeeded();
+      await expect(target).toBeVisible();
+      await targetCheckbox.check();
+      await expect(target).not.toBeVisible();
+      await targetCheckbox.uncheck();
+      await expect(target).toBeVisible();
+    });
+  });
 });
