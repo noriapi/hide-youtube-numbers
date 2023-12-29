56926c655092d5e7c639e91429f7ee07a4bc599a
noriapi <70106808+noriapi@users.noreply.github.com>
2023-12-29 21:29:53 +0900

    chore(ci): use multiple reporters in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
 
   // Reporter to use
-  reporter: process.env.CI ? "github" : "html",
+  reporter: process.env.CI ? [["github"], ["html"]] : "html",
 
   use: {
     // Collect trace when retrying the failed test.
