bb5245a43e8db2345d4e464e546eea0bc6690622
noriapi <70106808+noriapi@users.noreply.github.com>
2024-01-03 01:31:50 +0900

    create submit command

--- scripts/submit.ts
+++ scripts/submit.ts
@@ -1,35 +1,48 @@
+import { Command, Option } from "@commander-js/extra-typings";
 import { publishExtension } from "publish-browser-extension";
 
-//
-//accounts.google.com/o/oauth2/auth?response_type=code&scope=https://www.googleapis.com/auth/chromewebstore&client_id=802073216874-2e0ltqurbqfrj76ictri90ui95v456h7.apps.googleusercontent.com&redirect_uri=urn:ietf:wg:oauth:2.0:oob
+const submitChrome = (opts: { dryRun: boolean }) => {
+  return publishExtension({
+    dryRun: opts.dryRun,
+    chrome: {
+      zip: `.output/hide-youtube-numbers-${process.env.npm_package_version}-chrome.zip`,
+      extensionId: "joboadjmcbnnneggpohpnmkblmmjiehh",
+      clientId: process.env.CHROME_CLIENT_ID ?? "",
+      clientSecret: process.env.CHROME_CLIENT_SECRET ?? "",
+      refreshToken: process.env.CHROME_REFRESH_TOKEN ?? "",
+      // publishTarget: "<default|trustedTesters>",
+      // skipSubmitReview: false,
+    },
+  });
+};
 
-publishExtension({
-  dryRun: true,
-  chrome: {
-    zip: `.output/hide-youtube-numbers-${process.env.npm_package_version}-chrome.zip`,
-    extensionId: "joboadjmcbnnneggpohpnmkblmmjiehh",
-    clientId: process.env.CHROME_CLIENT_ID ?? "",
-    clientSecret: process.env.CHROME_CLIENT_SECRET ?? "",
-    // refreshToken: "<gcp-refresh-token>",
-    // publishTarget: "<default|trustedTesters>",
-    // skipSubmitReview: false,
-  },
-  firefox: {
-    zip: `.output/hide-youtube-numbers-${process.env.npm_package_version}-firefox.zip`,
-    sourcesZip: `.output/hide-youtube-numbers-${process.env.npm_package_version}-sources.zip`,
-    extensionId: "hide-youtube-numbers@noriapi.addon",
-    jwtIssuer: process.env.FIREFOX_JWT_ISSUER ?? "",
-    jwtSecret: process.env.FIREFOX_JWT_SECRET ?? "",
-    // channel: '<listed|unlisted>',
-  },
-  // edge: {
-  //   zip: 'dist/chrome.zip',
-  //   productId: "<edge-product-id>",
-  //   clientId: "<edge-client-id>",
-  //   clientSecret: "<edge-client-secret>",
-  //   accessTokenUrl: "<edge-access-token-url>",
-  //   skipSubmitReview: false,
-  // },
-})
-  .then((results) => console.log(results))
-  .catch((err) => console.error(err));
+const submitFirefox = (opts: { dryRun: boolean }) => {
+  return publishExtension({
+    dryRun: opts.dryRun,
+    firefox: {
+      zip: `.output/hide-youtube-numbers-${process.env.npm_package_version}-firefox.zip`,
+      sourcesZip: `.output/hide-youtube-numbers-${process.env.npm_package_version}-sources.zip`,
+      extensionId: "hide-youtube-numbers@noriapi.addon",
+      jwtIssuer: process.env.FIREFOX_JWT_ISSUER ?? "",
+      jwtSecret: process.env.FIREFOX_JWT_SECRET ?? "",
+      // channel: '<listed|unlisted>',
+    },
+  });
+};
+
+const main = () => {
+  const program = new Command()
+    .addOption(new Option("-d --dry-run", "dry run").default(false))
+    .addOption(
+      new Option("-b --browser <browser>", "browser to submit to")
+        .choices(["chrome", "firefox"] as const)
+        .default("chrome" as const),
+    )
+    .parse();
+
+  const options = program.opts();
+
+  options.browser === "chrome" ? submitChrome(options) : submitFirefox(options);
+};
+
+main();
